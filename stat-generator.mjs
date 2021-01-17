a8ea4279c0fb659a486b28456c0b49806386d329
Jeff Mahoney <jeff.t.mahoney@gmail.com>
2021-01-16 23:05:49 -0500

    cycle-count: make adjustments so that running iterations for testing is easier

---
+++ stat-generator.mjs
@@ -0,0 +1,36 @@
+import Matrix from './matrix-source.mjs'
+
+function cycle() {
+    let currentMatrix = []
+    let previousMatrix = Matrix.generate()
+
+    let currentPopulation = 0
+    let previousPopulation = previousMatrix.filter(x => x.alive === true).length
+
+    let iterations = 0
+    let cyclingShouldStillHappen = true
+
+    while (cyclingShouldStillHappen) {
+        currentMatrix = Matrix.advance(previousMatrix, Matrix.getLiveStatus, Matrix.getLifeCount)
+        currentPopulation = currentMatrix.filter(x => x.alive === true).length
+        iterations++
+
+        cyclingShouldStillHappen = previousPopulation !== currentPopulation
+
+        previousMatrix = currentMatrix
+        previousPopulation = currentPopulation
+        currentMatrix = []
+        currentPopulation = 0
+    }
+    return iterations
+}
+
+let total = 0
+for (let i = 1; i <= 100; i++) {
+    let currentTotal = cycle()
+    total += currentTotal
+    console.log(`iteration #${i}: ${currentTotal}`)
+}
+let avg = total / 100
+console.log('avg')
+console.log(avg)
